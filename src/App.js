d0925a5efa51681d91bcb3194f34bdc9af511d92
mmajudith <mmajudith2000@gmail.com>
2023-05-26 16:16:30 -0700

    style
    Adjust the image background

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import BgImage from './assets/bg.jpg';
 const App = () => {
 	return (
 		<div
-			className="w-full h-full m-auto py-14 lg:py-[85px] bg-no-repeat bg-[center_left] text-base font-normal lg:border-l-[25px] lg:border-r-[25px] lg:border-l-white lg:border-r-white lg:border-solid"
+			className="w-full h-full m-auto py-14 lg:py-[85px] bg-no-repeat bg-[center_left_14%] text-base font-normal lg:border-l-[25px] lg:border-r-[25px] lg:border-l-white lg:border-r-white lg:border-solid"
 			style={{ backgroundImage: `url(${BgImage})` }}
 		>
 			{' '}
